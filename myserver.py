21e12fd7460e139a8d509839276d5245e497b40d
Lisa-86 <pod.features@gmail.com>
2020-04-07 17:17:29 +0100

    Third commit - adding some stuff xx

--- myserver.py
+++ myserver.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
+
+from territories import teralloc, territories, ters
+
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def run_risk():
     return render_template("home.html")
+
+class TroopResource(Resource):
+    def get(self):
+        p1ters, p2ters = teralloc(ters)
+        return {'player 1 territories': p1ters, "player 2 territories": p2ters}
+
+api.add_resource(TroopResource, '/countries')
 
 if __name__ == '__main__':
     app.run()
---
+++ territories.py
@@ -0,0 +1,70 @@
+import random
+
+territories = {
+    # Oceania
+    'Alaska': [112, 160],
+    'Eastern Australia': [1461, 864],
+    'Western Australia': [1324, 854],
+    'New Guinea': [1444, 725],
+    'Indonesia': [1313, 673],
+    # Asia
+    'Siam': [1228, 554],
+    'India': [1110, 557],
+    'China': [1266, 450],
+    'Middle East': [944, 544],
+    'Afghanistan': [1029, 401],
+    'Mongolia': [1263, 350],
+    'Irkutsk': [1240, 256],
+    'Ural': [1046, 230],
+    'Siberia': [1129, 228],
+    'Yakutsk': [1268, 166],
+    'Kamchatka': [1404, 179],
+    'Japan': [1404, 386],
+    # Europe
+    'Ukraine': [905, 261],
+    'Scandinavia': [776, 221],
+    'Northern Europe': [766, 315],
+    'Southern Europe': [800, 381],
+    'Western Europe': [715, 385],
+    'Great Britain': [697, 305],
+    'Iceland': [660, 221],
+    # North America
+    'Alaska': [112,162],
+    'Northwest Territory': [261,167],
+    'Alberta': [258,250],
+    'Ontario': [352,261],
+    'Greenland': [603,94],
+    'Quebec': [495,266],
+    'Western United States': [271,330],
+    'Eastern United States': [390,377],
+    'Central America': [270,448],
+    # South America
+    'Venezuela': [389,548],
+    'Brazil': [490,643],
+    'Peru': [377,686],
+    'Argentina': [424,790],
+    # Africa
+    'North Africa': [708,554],
+    'Egypt':[828,506],
+    'East Africa': [880,625],
+    'Congo': [825,683],
+    'South Africa': [834,824],
+    'Madagascar':[950,826],
+    }
+
+ters = []
+for ter in territories:
+    ters.append(ter)
+
+def teralloc(ters):
+    playerno = 2
+    troopno = int(len(ters) / playerno)
+
+    p1ters = random.sample(ters, troopno)
+    p2ters = []
+
+    for ter in ters:
+        if ter not in p1ters:
+            p2ters.append(ter)
+
+    return p1ters, p2ters
