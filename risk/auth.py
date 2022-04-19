b5312f8c022b9ac0c295fdd96abb61d747fb5411
bieniekmat <bieniekmat@gmail.com>
2022-04-19 22:27:02 +0100

    relative import

--- risk/auth.py
+++ risk/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user
-from db import db
-from models import User
+from .db import db
+from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
