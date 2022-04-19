9546ea902cc95eb09709667e1c9a3442311ed38e
bieniekmat <bieniekmat@gmail.com>
2022-04-19 22:50:21 +0100

    root

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from risk.main import create_app
  
 #if __name__ == "__main__":
-risk_app = create_app('postgresql://vqqlbiorricpql:49e5dadbc8211fd137a5b0a726c87b6402d1c1807724d9c5bb09532bf70e9c83@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d85b64i6hth1nd', 'risk')
+risk_app = create_app('postgresql://vqqlbiorricpql:49e5dadbc8211fd137a5b0a726c87b6402d1c1807724d9c5bb09532bf70e9c83@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d85b64i6hth1nd', '/')
