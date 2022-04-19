40e19a5974825db4de44bab7c8cc46694acf272b
bieniekmat <bieniekmat@gmail.com>
2022-04-19 22:53:09 +0100

    newdb

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from risk.main import create_app
  
 #if __name__ == "__main__":
-risk_app = create_app('postgresql://vqqlbiorricpql:49e5dadbc8211fd137a5b0a726c87b6402d1c1807724d9c5bb09532bf70e9c83@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d85b64i6hth1nd', '/')
+risk_app = create_app('postgresql://tiegzyqfvrxfba:40845fab9f57e2c475ab5eed4b9f856092c8c1a8a7d803ce30a5ee40ab210df1@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d203t1604r6fr
+', '/')
