2700ff6f33dfc957776ec0cbc905351b17cb7510
bieniekmat <bieniekmat@gmail.com>
2022-04-19 22:00:38 +0100

    relative import

--- risk/main.py
+++ risk/main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, session
 from flask_login import LoginManager, current_user
 from flask_restful import Resource, Api
 
-from territories import teralloc, teralloc_db, territories
+from .territories import teralloc, teralloc_db, territories
 from risk_utils import reinforcements, diceroll, winGame
 from db import db
 from models import *
