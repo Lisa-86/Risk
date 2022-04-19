add29605bbe0604dc28b68fa9fe07d48f2641e65
bieniekmat <bieniekmat@gmail.com>
2022-04-19 22:56:01 +0100

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,3 +3,4 @@ from risk.main import create_app
 #if __name__ == "__main__":
 risk_app = create_app('postgresql://tiegzyqfvrxfba:40845fab9f57e2c475ab5eed4b9f856092c8c1a8a7d803ce30a5ee40ab210df1@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d203t1604r6fr
 ', '/')
+
